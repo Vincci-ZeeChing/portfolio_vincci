426ea67627ac8afd1a8f3325bf6836c4a193448e
vincci.chye <vincci.chye@redone.com.my>
2024-01-25 18:46:16 +0800

    :tada: Testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Homepage from "./view/homepage";
-import About from "./view/about";
-import Services from "./view/services";
-import Skill from "./view/skill";
-import Contact from "./view/contact";
+// import {BrowserRouter, Route, Routes} from "react-router-dom";
+// import Homepage from "./view/homepage";
+// import About from "./view/about";
+// import Services from "./view/services";
+// import Skill from "./view/skill";
+// import Contact from "./view/contact";
 
 
 function App() {
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Homepage/>}/>
-                    <Route path="/homepage" element={<Homepage/>}/>
-                    <Route path="/about" element={<About/>}/>
-                    <Route path="/skill" element={<Skill/>}/>
-                    <Route path="/services" element={<Services/>}/>
-                    <Route path="/contact" element={<Contact/>}/>
-                </Routes>
-            </BrowserRouter>
+            hello
+            {/*<BrowserRouter>*/}
+            {/*    <Routes>*/}
+            {/*        <Route path="/" element={<Homepage/>}/>*/}
+            {/*        <Route path="/homepage" element={<Homepage/>}/>*/}
+            {/*        <Route path="/about" element={<About/>}/>*/}
+            {/*        <Route path="/skill" element={<Skill/>}/>*/}
+            {/*        <Route path="/services" element={<Services/>}/>*/}
+            {/*        <Route path="/contact" element={<Contact/>}/>*/}
+            {/*    </Routes>*/}
+            {/*</BrowserRouter>*/}
         </div>
     );
 }
