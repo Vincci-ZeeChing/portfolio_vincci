24b4c5d48db6773453f015c14b2947564132b8d7
vincci.chye <vincci.chye@redone.com.my>
2024-01-25 17:56:59 +0800

    :white_check_mark: Test Deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, Vincci here! I'm learning React.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello, Vincci here
+          Learn React
         </a>
       </header>
     </div>
