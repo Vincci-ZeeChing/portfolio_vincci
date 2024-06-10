fd0970f60971242798c5b9603f475d7b641010db
Vincci-ZeeChing <vincci-zeeching@outlook.com>
2024-06-10 16:55:07 +0800

    :lipstick: Homepage animation

--- src/view/homepage/index.js
+++ src/view/homepage/index.js
@@ -1,9 +1,7 @@
-// Homepage.js
 import React from 'react';
 import Layout from '../layout';
 import './index.css'; // Import the CSS file
 import VincciImage from '../../component/image/Vincci.png'; // Use a different name if necessary
-// import { IoMdInformationCircleOutline } from "react-icons/io";
 import { AiOutlineLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
 
 const Homepage = () => {
@@ -27,19 +25,16 @@ const Homepage = () => {
                            rel="noopener noreferrer">
                             <AiFillMail className="contact-icon"/>
                         </a>
-                        {/* <button onClick={() => navigateTo('/about')} className="about-button">About Me <IoMdInformationCircleOutline /></button> */}
                     </div>
 
                     {/* Right side with smaller and circular photo */}
                     <div className="image-container"> {/* Apply the image-container class */}
-                        {/* Add your photo here */}
                         <img
                             src={VincciImage}
                             alt="Vincci"
                             className='image'
                         />
                     </div>
-
                 </div>
             </div>
         </Layout>
